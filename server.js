c4671e597c1c46a81c4ac8c4c88ae6e8f5cad481
John Roller <jtroller33@gmail.com>
2022-08-16 22:30:03 -0500

    HTML routes in server.js started

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
+// const api = require('./routes/indes.js')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,8 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+// app.use('/api', api);
+app.use(express.static('public'));
 
-// sync sequelize models to the database, then turn on the server
+// GET route for landing page                             
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// wildcard route for 404 page. should ALWAYS be below ALL other routes.
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/404.html'))
+// );
+
+
+// TODO: sync sequelize models to the database, then turn on the server
+
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
