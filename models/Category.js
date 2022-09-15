eaf7bf46b863393ce5ecca8ec7b83504baa94d53
John Roller <jtroller33@gmail.com>
2022-09-15 17:19:52 -0500

    category model

--- models/Category.js
+++ models/Category.js
@@ -1,14 +1,23 @@
-// 11
-
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Category
+    // id - Integer - Doesn't allow null values -Set as primary key - Uses auto increment
+    // category_name - String - Doesn't allow null values
   },
   {
     sequelize,
